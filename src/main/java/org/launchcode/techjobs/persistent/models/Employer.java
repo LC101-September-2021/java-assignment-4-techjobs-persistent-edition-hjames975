f248e9011729eeef33422d7860b8740ec0a5118f
James <jhfam.jh@gmail.com>
2022-03-24 20:01:11 -0500

    Final

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,50 @@
 package org.launchcode.techjobs.persistent.models;
 
+
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+//    @Id
+//    @GeneratedValue
+//    private int id;
+
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private List<Job> jobs = new ArrayList<>();
+
+    @NotNull
+    @Size(min = 1, max = 100, message = "Location must be between 1 and 100 characters long")
+    public String location;
+
+    public Employer(){}
+
+//    public Employer(int id, String location){
+//        super();
+//        this.id = id;
+//        this.location = location;
+//    }
+
+
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
 
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,46 @@
 package org.launchcode.techjobs.persistent.models;
 
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+//    @Id
+//    @GeneratedValue
+//    private int id;
+
+    @ManyToMany(mappedBy="skills")
+    private List<Job> jobs = new ArrayList<>();
+
+    @Size(min = 0, max = 1000)
+    private String description;
+
+    public Skill(){}
+
+//    public Skill(int id, String description){
+//        super();
+//    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
 }
